3bd8b86e1c76fc7aadf97a9b985bcc79ecbf6cf1
Rodevp <rodriguezrodrigor12@gmail.com>
2022-05-15 11:31:35 -0500

    creando el handle error

---
+++ src/middlewares/handle-error.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express' 
+
+//TODO: buscar el tipo de error:
+
+export const ErrorHandleMiddleware = (
+    err: any,
+    _req: Request,
+    res: Response,
+    next: NextFunction
+    ) => {
+        const customError = err.statusCode
+        ? { statusCode: err.statusCode, message: err.message }
+        : { message: err.message } 
+
+    const status = customError.statusCode ? customError.statusCode : 500
+
+    return res.status(status).json({
+        message: customError.message
+    })
+}
