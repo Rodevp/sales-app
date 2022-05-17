8ca22578f82c9150a64f3ca196609bafb316d239
Rodevp <rodriguezrodrigor12@gmail.com>
2022-05-17 08:26:22 -0500

    agregando rate-limiter

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { Request, Response, NextFunction } from 'express'
+import { Request, Response } from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
 import sequelize from './database'
@@ -7,6 +7,7 @@ import morgan from 'morgan'
 import fileUpload from 'express-fileupload'
 import { errorHandleMiddleware } from './middlewares/handle-error'
 import { authenticationMiddleware } from './middlewares/auth-middleware'
+import rateLimiter from 'express-rate-limit'
 
 //routes import
 import authRouter from './routes/auth'
@@ -23,11 +24,19 @@ import './api/sales/model'
 const PORT = process.env.ENV || 3001
 const app = express()
 
-app.use(cors())
-app.use(helmet())
-app.use(express.json())
-app.use(fileUpload())
+app.use( cors() )
+app.use( helmet() )
+app.use( express.json() )
+app.use( fileUpload() )
 app.use(morgan('dev'))
+
+const limiter = rateLimiter({
+    windowMs: 30 * 60 * 1000,
+    max: 1000,
+    standardHeaders: true,
+    legacyHeaders: true
+})
+
 
 app.get('/api', (_req: Request, res: Response) => {
     res.send('<h1>Api on fire</>')
@@ -36,7 +45,7 @@ app.get('/api', (_req: Request, res: Response) => {
 
 //routers
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/product', authenticationMiddleware, productRouter)
+app.use('/api/v1/product', [authenticationMiddleware, limiter], productRouter)
 app.use('/api/v1/sales', authenticationMiddleware, salesRouter)
 app.use('/api/v1/admin', authenticationMiddleware, adminRouter)
 
